ff5337467d4d7e964faa71b056d63d3e764e6bd6
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-04-06 16:43:59 +0300

    eror1

--- scripts/index.js
+++ scripts/index.js
@@ -1,181 +1,155 @@
-
 /*----------Переменные открытие и закрытие попапов----------------*/
-const openPopUp = document.querySelector('.edit-button');
-const closePopUp = document.querySelector('.popup-close');
-const popUp = document.querySelector('.popup');
-const profInfo = document.querySelector('.form__input-container_prof-info');
-const nameInfo = document.querySelector('.form__input-container_name-info');
-let userName = document.querySelector('.profile__name');
-let userInfo = document.querySelector('.profile__job');
-const submitButton = document.querySelector('.form__button-submit');
-const openTrawelInfo = document.getElementById('trawelInfo');
-const addCardButton = document.querySelector('.add-button');
-const closeTrawelCard = document.getElementById('closeTravel');
+const popUpOpen = document.querySelector(".edit-button");
+const popUpClose = document.querySelector(".popup-close");
+const popUpUserInfo = document.querySelector(".popup");
+const profInfo = document.querySelector(".form__input-container_prof-info");
+const nameInfo = document.querySelector(".form__input-container_name-info");
+const userName = document.querySelector(".profile__name");
+const userInfo = document.querySelector(".profile__job");
+const trawelInfoOpen = document.getElementById("trawelInfo");
+const cardButtonAdd = document.querySelector(".add-button");
+const trawelCardclose = document.getElementById("closeTravel");
+
+/*---------------Переменные для создание новых карточек-----------------------*/
+const template = document.getElementById("card-template");
+const newNameCard = document.querySelector(".card__caption");
+const newUrlInfo = document.querySelector(".card__pic");
+const cardContainer = document.querySelector(".grid-gallery");
+const formTravel = document.getElementById("popUpTravel");
+const namePicAdd = document.getElementById("cardInfo");
+const urlAdd = document.getElementById("urlInfo");
+const picOpen = document.getElementById("zoomPic");
+const picZoom = document.querySelector(".popup__zoom-pic");
+const captureZoom = document.querySelector(".popup__capture");
+const closePic = document.querySelector(".popup-close_pic");
 
 
-function openModal () {
-    popUp.classList.add('open');
-    nameInfo.value = userName.textContent
-    profInfo.value = userInfo.textContent
+function openModal(selector) {
+ selector.classList.add("popup_opened");
+}
+function closeModal(selector) {
+  selector.classList.remove("popup_opened");
 }
 
-function closeModal () {
-  popUp.classList.remove('open');
+function propfilePopupOpen(){
+  openModal(popUpUserInfo);
+  nameInfo.value = userName.textContent;
+  profInfo.value = userInfo.textContent;
 }
 
-openPopUp.addEventListener('click', () => openModal());
+popUpOpen.addEventListener("click", () => propfilePopupOpen());
 
-closePopUp.addEventListener('click', () => closeModal())
+popUpClose.addEventListener("click", () => closeModal(popUpUserInfo));
 
-document.addEventListener('keydown', (e) => {
-	if( e.key === "Escape" ){ 
-       closeModal();
-	}
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeModal(popUpUserInfo);
+  }
 });
 
 /*------ отправка формы---------------- */
-let submitForm = document.querySelector('.form');
- 
-submitForm.addEventListener('submit', (evt) =>{
+const formSubmit = document.querySelector(".form");
+
+formSubmit.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  closeModal();})
+  closeModal(popUpUserInfo);
+});
 
 /*---------заполнение информации пользователя-------------*/
 
-function saveInfoModal (){
-  userName.textContent = nameInfo.value
-  userInfo.textContent = profInfo.value
+function saveInfoModal() {
+  userName.textContent = nameInfo.value;
+  userInfo.textContent = profInfo.value;
 }
 
-submitForm.addEventListener('click', () => saveInfoModal())
+formSubmit.addEventListener("click", () => saveInfoModal());
 
 /*-------вкл/выкл попап добавления картинок----------*/
 
-function openModalTrawelCard(){
-  openTrawelInfo.classList.add('open');
-};
-
-function closeModalTrawelCard(){
-  openTrawelInfo.classList.remove('open');
-};
-
-document.addEventListener('keydown', (e) => {
-	if( e.key === "Escape" ){ 
-    closeModalTrawelCard();
-	}
-});
-
-addCardButton.addEventListener('click', () => openModalTrawelCard());
-closeTrawelCard.addEventListener('click', () => closeModalTrawelCard());
-
-
-/*---------------Переменные для создание новых карточек-----------------------*/
-let newNameCard = document.querySelector('.card__caption');
-let newUrlInfo = document.querySelector('.card__pic');
-const cardContainer = document.querySelector('.grid-gallery');
-const formTravel = document.getElementById('popUpTravel');
-let creatCard = document.getElementById('creatCard');
-let addNamePic = document.getElementById('cardInfo');
-let addUrl = document.getElementById('urlInfo');
-let taskNameCard = document.querySelector ('.card__caption');
-let taskUrlInfo = document.querySelector ('.card__pic');
-const subFormTravel = document.getElementById('creatCard')
-const openPic = document.getElementById('zoomPic');
-const smallPic = document.querySelector ('.card_smallPic');
-const closePic = document.getElementById('closePic');
-const zoomPic = document.querySelector('.popup__zoom-pic');
-const zoomCapture = document.querySelector('.popup__capture');
-
+cardButtonAdd.addEventListener("click", () => openModal(trawelInfoOpen));
+trawelCardclose.addEventListener("click", () => closeModal(trawelInfoOpen));
 
 
 const photoArray = [
-  {name: 'Коста-Рика', src:'./image/Costa_Rica.jpg'}, 
-  {name: 'Греция', src:'./image/Greece.jpg'},
-  {name: 'Австралия', src:'./image/Blue_Mountains_Australia.jpg'},
-  {name: 'Индия', src:'./image/India1.jpg'},
-  {name: 'Франция', src:'./image/Isola_france.jpg'},
-  {name: 'Рио-де-Жанейро', src:'./image/Rio_de_Janeiro.jpg'},
+  { 
+    name: "Коста-Рика", 
+    src: "./image/Costa_Rica.jpg"
+  },
+  { 
+    name: "Греция",
+    src: "./image/Greece.jpg"
+  },
+  { 
+    name: "Австралия",
+    src: "./image/Blue_Mountains_Australia.jpg"
+  },
+  { 
+    name: "Индия",
+    src: "./image/India1.jpg"
+  },
+  { 
+    name: "Франция",
+    src: "./image/Isola_france.jpg"
+  },
+  { 
+    name: "Рио-де-Жанейро",
+    src: "./image/Rio_de_Janeiro.jpg"
+  },
 ];
 
-function openModalPic (card){
-  openPic.classList.add('open');
-  zoomCapture.textContent = card.name;
-  zoomPic.src = card.src;
+function openModalPic(card) {
+  picOpen.classList.add("popup_opened");
+  captureZoom.textContent = card.name;
+  picZoom.src = card.src;
+  picZoom.alt = card.src;
+
 } // открыть картинку
 
-function closeModalPic (){
-  openPic.classList.remove('open');
+function closeModalPic() {
+  picOpen.classList.remove("popup_opened");
 } // закрыть картинку
 
-const creatBlock = (card) =>{
-    const template = document.getElementById('card-template');
-    const task = template.content.querySelector('.card').cloneNode(true);
-    task.querySelector('.card__caption').textContent = card.name;
-    task.querySelector('.card__pic').src = card.src;
-    task.querySelector('.card__pic').alt = card.name;
 
-    task.querySelector('.card__delete').addEventListener('click', () => {
-      task.remove();
-    });  // слушатель удаления карточки
+const creatBlock = (card) => {
+  const task = template.content.querySelector(".card").cloneNode(true);
+  const cardPic = task.querySelector('.card__pic');
+  task.querySelector(".card__caption").textContent = card.name;
+  cardPic.src = card.src;
+  cardPic.alt = card.name;
 
-    task.querySelector('.button-like').addEventListener('click', (evt) => {
-      evt.target.classList.toggle('button-like');
-      evt.target.classList.toggle('button-like_active');;
-    }); // слушатель лайка
+  task.querySelector(".card__delete").addEventListener("click", () => {
+    task.remove();
+  }); // слушатель удаления карточки
 
-    //вкл/выкл большое изображение
-    task.querySelector('.card_smallPic').addEventListener('click', () => openModalPic(card));
-    const closePic = document.querySelector('.popup-close_pic').addEventListener('click', () => closeModalPic ());
+  task.querySelector(".button-like").addEventListener("click", (evt) => {
+    evt.target.classList.toggle("button-like_active");
+  }); // слушатель лайка
 
-    return task
-  }
+  //вкл/выкл большое изображение
+  cardPic.addEventListener("click", () => openModalPic(card));
+  closePic.addEventListener("click", () => closeModal(picOpen));
 
-  const renderCard = (card) => {
-    cardContainer.prepend(creatBlock(card));
-  };
+  return task;
+};
 
-  const elements = photoArray.map((card) => {
-    return creatBlock(card);
-  });
+const cardRender = (card) => {
+  cardContainer.prepend(creatBlock(card));
+};
+
+const elements = photoArray.map((card) => {
+  return creatBlock(card);
+});
 
 /*---------Добавление карточек через кнопку Создать------*/
 
 const addCard = (event) => {
   event.preventDefault();
-  const card = {name: addNamePic.value, src: addUrl.value};
-  renderCard(card);
-  addNamePic.value = '';
-  addUrl.value = '';
-  closeModalTrawelCard()
+  const card = { name: namePicAdd.value, src: urlAdd.value };
+  cardRender(card);
+  closeModal(trawelInfo);
+  namePicAdd.value = "";
+  urlAdd.value = "";
 };
 
 cardContainer.append(...elements);
-formTravel.addEventListener('submit', addCard);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+formTravel.addEventListener("submit", addCard);
