cb1164c40aecfe49c389fa333b40e086fcd110ff
Anastasiya Brachunova <abrachunova@yandex.ru>
2022-04-12 19:57:32 +0300

    valid

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-/*----------Переменные открытие и закрытие попапов----------------*/
+// /*----------Переменные открытие и закрытие попапов----------------*/
 const userInfoOpen = document.querySelector(".edit-button");
 const userInfoClose = document.getElementById("closeUserInfo");
 const popUpUserInfo = document.getElementById("profilePopup");
@@ -9,10 +9,8 @@ const userInfo = document.querySelector(".profile__job");
 const trawelInfoOpen = document.getElementById("trawelInfo");
 const cardButtonAdd = document.querySelector(".add-button");
 const trawelCardclose = document.getElementById("closeTravel"); 
-
 /*---------------Переменные для создание новых карточек-----------------------*/
 const template = document.getElementById("card-template");
-const newNameCard = document.querySelector(".card__caption");
 const cardContainer = document.querySelector(".grid-gallery");
 const formTravel = document.getElementById("popUpTravel");
 const namePicAdd = document.getElementById("cardInfo");
@@ -23,13 +21,34 @@ const captureZoom = document.querySelector(".popup__capture");
 const closePic = document.querySelector(".popup-close_pic");
 
 
-function openModal(popupId) {
- popupId.classList.add("popup_opened");
-}
-function closeModal(popupId) {
- popupId.classList.remove("popup_opened");
-}
 
+
+
+
+/*----универсальное открытие и закрытие модальных окон-------*/
+
+ function openModal(popupId) {
+  popupId.classList.add("popup_opened");
+ }
+ function closeModal(popupId) {
+  popupId.classList.remove("popup_opened");
+ }
+
+function escClose(evt){
+  if (evt.key === "Escape") {
+    const popUpOpened = document.querySelector(".popup_opened");
+     closeModal(popUpOpened);
+   } 
+ }
+ document.addEventListener("keydown",  escClose);
+
+function overlayClose(evt){
+  const popUpOpened = document.querySelector(".popup_opened");
+  evt.target.classList.toggle("popup_opened");
+}
+document.addEventListener("click", overlayClose);
+
+/*-----------------------------------------------------------*/
 function propfilePopUpOpen(){
   nameInfo.value = userName.textContent;
   profInfo.value = userInfo.textContent;
@@ -137,3 +156,64 @@ const addCard = (event) => {
 
 cardContainer.append(...elements);
 formTravel.addEventListener("submit", addCard);
+
+
+/*------------- ВАЛИДАЦИЯ ФОРМ--------------*/
+
+const addUserForm = document.forms.user;
+const addTravelForm = document.forms.travel;
+
+
+const validateInput = (input) =>{
+  const errorElement = input.parentNode.querySelector(`#${input.id}-error`);
+  // валидируем инпут
+  errorElement.textContent = input.validationMessage;
+
+}
+
+const enableButton = (button, inactiveButtonClass) =>{
+  button.disabled = false;
+  button.classList.remove(inactiveButtonClass);
+}
+
+const disableButton = (button, inactiveButtonClass) =>{
+  button.disabled = true;
+  button.classList.add(inactiveButtonClass);
+}
+
+const travelButton = addTravelForm.querySelector('.form__button-submit');
+disableButton(travelButton, 'form__button-submit_invalid');
+
+
+const setButtonState = (button, isValid) =>{
+  if (isValid){
+    enableButton(button, 'form__button-submit_invalid');
+    
+  } else{
+    disableButton(button, 'form__button-submit_invalid');
+  }
+}
+const handleInput = (event) =>{   // обработчик поля ввода
+  const currentForm = event.currentTarget;
+  const input = event.target;
+  const submitButton = currentForm.querySelector('.form__button-submit');
+
+  validateInput(input);
+  setButtonState(submitButton, currentForm.checkValidity());
+}
+
+const handleSubmit = (event) =>{ // обработчик сабмита
+ event.preventDefault();
+
+ const currentForm = event.target;
+
+ if (currentForm.checkValidity()){
+  currentForm.reset();
+ }
+}
+addUserForm.addEventListener('submit', handleSubmit);
+addTravelForm.addEventListener('submit', handleSubmit);
+
+
+addUserForm.addEventListener('input', handleInput);
+addTravelForm.addEventListener('input', handleInput);
